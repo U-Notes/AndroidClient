45f8e8154a4982362c9a90ba6e672d1b66683131
wangtianle1 <2972865937@qq.com>
2023-08-02 20:46:21 +0800

    feat:BitmapUtils,CameraUtils,ToastUtils bug:permission lead to splash screen

---
+++ app/src/main/java/com/example/unotes/constant/Constant.java
@@ -0,0 +1,8 @@
+package com.example.unotes.constant;
+
+public class Constant {
+    //启动相机标识
+    public static final int TAKE_PHOTO = 1;
+    //启动相册标识
+    public static final int SELECT_PHOTO = 2;
+}
